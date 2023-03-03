24863e854eba745a85ada7a9bba978dd6c2cf025
kevinbre <bredeliskev@gmail.com>
2023-03-03 14:44:18 -0300

    Fixed svg props

--- src/components/BounceButton.tsx
+++ src/components/BounceButton.tsx
@@ -4,9 +4,9 @@ export const BounceButton: React.FC = () => {
       <svg
         className="w-6 h-6 text-violet-500"
         fill="none"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
         viewBox="0 0 24 24"
         stroke="currentColor"
       >
